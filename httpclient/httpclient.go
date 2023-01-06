52035d37815b934673fbf203f49f77b5b6b5d0e9
viktor <vwelbers@cocus.com>
2023-01-06 12:58:47 +0100

    feat: Add Authentication checking endpoint

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -1,13 +1,8 @@
 package httpclient
 
 import (
-	"bytes"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 type ImageHttpClient struct {
@@ -30,36 +25,6 @@ func (h ImageHttpClient) GetImageFromQueue(uuid string) (*http.Response, error) 
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("Authorization", h.apiKey)
-	return h.client.Do(req)
-}
-
-func (h ImageHttpClient) UploadImage(clientId string, imageFile *multipart.FileHeader, contentType string) (*http.Response, error) {
-	log.Println("Uploading image to image service with for client id: " + clientId)
-
-	image, err := imageFile.Open()
-	if err != nil {
-		return nil, err
-	}
-	defer image.Close()
-
-	formData := url.Values{}
-	formData.Add("file", imageFile.Filename)
-	file, err := imageFile.Open()
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	buf := new(bytes.Buffer)
-	io.Copy(buf, file)
-	formData.Add("file", buf.String())
-
-	req, err := http.NewRequest("POST", h.url+"/upload/"+clientId, strings.NewReader(formData.Encode()))
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "multipart/form-data")
 	req.Header.Add("Authorization", h.apiKey)
 	return h.client.Do(req)
 }
