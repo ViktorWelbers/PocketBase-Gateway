d0df662c9c4db535eb671f51db9b505c9d0b9c33
ViktorWelbers <v.welbers@gmx.de>
2023-01-07 10:45:32 +0100

    feat: add health check to service

--- main.go
+++ main.go
@@ -11,7 +11,28 @@ import (
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"log"
+	"net/http"
 )
+
+func AddHealthcheck(app *pocketbase.PocketBase) {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		_, err := e.Router.AddRoute(echo.Route{
+			Method: "GET",
+			Path:   "/health",
+			Handler: func(c echo.Context) error {
+				return c.JSON(http.StatusOK, map[string]string{"status": "OK"})
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				apis.ActivityLogger(app),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
 
 func addHandlerToRouter(app *pocketbase.PocketBase, handler handlers.Handler, method string, path string) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
@@ -34,6 +55,7 @@ func addHandlerToRouter(app *pocketbase.PocketBase, handler handlers.Handler, me
 
 func main() {
 	app := pocketbase.New()
+	AddHealthcheck(app)
 	httpClient := httpclient.NewImageHttpClient(config.ImageServiceUrl, config.ImageServiceApiKey)
 	queueClient := messaging.NewQueueClient(config.ReceiveQueueName, config.PublishQueueName, config.BusConnectionString)
 	addHandlerToRouter(app, handlers.NewMessageProducerHandler(queueClient), "GET", "api/send_message")
