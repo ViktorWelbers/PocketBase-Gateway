187efec6bd2de87fd3e0d1b4f05726679a4999e5
ViktorWelbers <v.welbers@gmx.de>
2023-04-21 22:14:18 +0200

    refactor: update project structure
    - the project structure is now in line with a commonly used one in go projects

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,8 +1,8 @@
 package handlers
 
 import (
-	"api-gateway/httpclient"
-	"api-gateway/messaging"
+	"api-gateway/internal/httpclient"
+	"api-gateway/internal/messaging"
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/models"
@@ -13,43 +13,29 @@ import (
 	"time"
 )
 
-type Handler interface {
-	Handle(c echo.Context) error
-}
-
-type MessageProducer struct {
-	// The client to use for the handler
-	QueueClient *messaging.QueueClient
-}
+type MessageProducer messaging.QueueClient
 
 type ImageDownload struct {
-	// The client to use for the handler
 	QueueClient     *messaging.QueueClient
 	ImageHttpClient *httpclient.ImageHttpClient
 }
 
-type Authentication struct {
-	// The client to use for the handler
-	ImageHttpClient *httpclient.ImageHttpClient
+type Authentication struct{}
+
+func NewMessageProducerHandler(queueClient *messaging.QueueClient) *MessageProducer {
+	messageProducer := MessageProducer(*queueClient)
+	return &messageProducer
 }
 
-func NewMessageProducerHandler(queueClient *messaging.QueueClient) Handler {
-	return &MessageProducer{
-		QueueClient: queueClient,
-	}
-}
-
-func NewImageDownloadHandler(queueClient *messaging.QueueClient, httpClient *httpclient.ImageHttpClient) Handler {
+func NewImageDownloadHandler(queueClient *messaging.QueueClient, httpClient *httpclient.ImageHttpClient) *ImageDownload {
 	return &ImageDownload{
 		QueueClient:     queueClient,
 		ImageHttpClient: httpClient,
 	}
 }
 
-func NewAuthenticationHandler(httpClient *httpclient.ImageHttpClient) Handler {
-	return &Authentication{
-		ImageHttpClient: httpClient,
-	}
+func NewAuthenticationHandler() *Authentication {
+	return &Authentication{}
 }
 
 func (h *ImageDownload) Handle(c echo.Context) error {
@@ -113,7 +99,7 @@ func (h *MessageProducer) Handle(c echo.Context) error {
 		ModelType:      modelType,
 		Token:          c.Request().Header.Get("Authorization"),
 	}
-	err := message.Send(h.QueueClient)
+	err := message.Send((*messaging.QueueClient)(h))
 	if err != nil {
 		return apis.NewBadRequestError("Queue was not able to send message", err.Error())
 	}
