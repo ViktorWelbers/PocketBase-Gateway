bed62b5b13c7df14ac2c5fe4bb40e2831dbec729
ViktorWelbers <v.welbers@gmx.de>
2023-04-21 23:29:06 +0200

    refactor: update code and add additional interfaces to make them more testable

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"api-gateway/internal/httpclient"
 	"api-gateway/internal/messaging"
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase/apis"
@@ -13,36 +12,32 @@ import (
 	"time"
 )
 
-type MessageProducer messaging.QueueClient
-
-type ImageDownload struct {
-	QueueClient     *messaging.QueueClient
-	ImageHttpClient *httpclient.ImageHttpClient
+type imageQueueClient interface {
+	ReceiveMessage(messageId string) string
+	SendMessage(message messaging.Message) error
 }
 
-type Authentication struct{}
-
-func NewMessageProducerHandler(queueClient *messaging.QueueClient) *MessageProducer {
-	messageProducer := MessageProducer(*queueClient)
-	return &messageProducer
+type httpClient interface {
+	GetImageFromQueue(uuid string) (*http.Response, error)
 }
 
-func NewImageDownloadHandler(queueClient *messaging.QueueClient, httpClient *httpclient.ImageHttpClient) *ImageDownload {
+func NewImageDownloadHandler(queueClient imageQueueClient, httpClient httpClient) *ImageDownload {
 	return &ImageDownload{
-		QueueClient:     queueClient,
-		ImageHttpClient: httpClient,
+		ImageQueueClient: queueClient,
+		ImageHttpClient:  httpClient,
 	}
 }
 
-func NewAuthenticationHandler() *Authentication {
-	return &Authentication{}
+type ImageDownload struct {
+	ImageQueueClient imageQueueClient
+	ImageHttpClient  httpClient
 }
 
 func (h *ImageDownload) Handle(c echo.Context) error {
 	log.Print("Received request to retrieve image")
 	messageId := c.QueryParam("message_id")
 
-	newUuid := messaging.ReceiveMessage(h.QueueClient, messageId)
+	newUuid := h.ImageQueueClient.ReceiveMessage(messageId)
 	if newUuid == "" {
 		return c.String(http.StatusAccepted, "Message not ready yet")
 	}
@@ -64,6 +59,12 @@ func (h *ImageDownload) Handle(c echo.Context) error {
 	return c.Blob(http.StatusOK, resp.Header.Get("Content-Type"), image)
 }
 
+func NewAuthenticationHandler() *Authentication {
+	return &Authentication{}
+}
+
+type Authentication struct{}
+
 func (h *Authentication) Handle(c echo.Context) error {
 	authRecord, _ := c.Get(apis.ContextAuthRecordKey).(*models.Record)
 	if authRecord == nil {
@@ -75,6 +76,16 @@ func (h *Authentication) Handle(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"client_id": userName,
 	})
+}
+
+type MessageProducer struct {
+	ImageQueueClient imageQueueClient
+}
+
+func NewMessageProducerHandler(queueClient imageQueueClient) *MessageProducer {
+	return &MessageProducer{
+		ImageQueueClient: queueClient,
+	}
 }
 
 func (h *MessageProducer) Handle(c echo.Context) error {
@@ -89,7 +100,7 @@ func (h *MessageProducer) Handle(c echo.Context) error {
 	strengthFloat, _ := strconv.ParseFloat(strength, 32)
 	promptGuidanceFloat, _ := strconv.ParseFloat(promptGuidance, 32)
 	messageId := strconv.FormatInt(time.Now().Unix(), 10) + "-" + uuid
-	// Send the message to the queue using the query parameters
+	// SendMessage the message to the queue using the query parameters
 	message := messaging.Message{
 		Prompt:         prompt,
 		PromptGuidance: promptGuidanceFloat,
@@ -99,11 +110,11 @@ func (h *MessageProducer) Handle(c echo.Context) error {
 		ModelType:      modelType,
 		Token:          c.Request().Header.Get("Authorization"),
 	}
-	err := message.Send((*messaging.QueueClient)(h))
+	err := h.ImageQueueClient.SendMessage(message)
 	if err != nil {
 		return apis.NewBadRequestError("Queue was not able to send message", err.Error())
 	}
-	// Send Response
+	// SendMessage Response
 	return c.JSON(http.StatusOK, map[string]string{
 		"messageId": messageId,
 	})
